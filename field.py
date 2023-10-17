01e2463c74cced34cf54b75281b6ede1179cb4de
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-10-17 21:50:03 +0900

    사진이 대량 로드되고 draw가 엄청나게 많이 되고있는 것을 확인하고 수정

--- field.py
+++ field.py
@@ -5,41 +5,51 @@ from pico2d import load_image
 class Running_track:
     num = -1
     command = []
+    image = None
+    obstacle = None
+    arrow = None
+    line = None
     def __init__(self, player):
-        self.image = load_image('running_track.png')
-        self.obstacle = load_image('obstacle.png')
-        self.arrow = load_image('arrow.png')
-        self.line = load_image('finishline.png')
+        if Running_track.image  == None:
+            Running_track.image = load_image('running_track.png')
+        if Running_track.obstacle  == None:
+            Running_track.obstacle = load_image('obstacle.png')
+        if Running_track.arrow  == None:
+            Running_track.arrow = load_image('arrow.png')
+        if Running_track.line  == None:
+            Running_track.line = load_image('finishline.png')
         Running_track.num += 1
         self.track_num = Running_track.num
         self.player = player
         self.mode = 2
 
     def draw(self):
-        self.image.clip_draw(26, 126, 254, 100, 254 * self.track_num - self.player.camera_x, 200, 254, 500)
-        self.image.clip_draw(28, 236, 208, 64, 1024 * (self.track_num // 4) - self.player.camera_x, 500, 1024, 200)
-        self.line.clip_composite_draw(0, 365, 840, 130, math.pi / 2, '', 200 - self.player.camera_x, 190, 250, 100)
-        self.line.clip_composite_draw(0, 365, 840, 130, math.pi / 2, '', 5000 - self.player.camera_x, 190, 250, 100)
-        for i in range(1000, 5000 - 1, 500):
-           self.obstacle.draw(i - self.player.camera_x, 120, 100, 100)
-        self.arrow_draw()
+        Running_track.image.clip_draw(26, 126, 254, 100, 254 * self.track_num - self.player.camera_x, 200, 254, 500)
+        Running_track.image.clip_draw(28, 236, 208, 64, 1024 * (self.track_num // 4) - self.player.camera_x, 500, 1024, 200)
+        if self.track_num == 20:
+            Running_track.line.clip_composite_draw(0, 365, 840, 130, math.pi / 2, '', 200 - self.player.camera_x, 190, 250, 100)
+            Running_track.line.clip_composite_draw(0, 365, 840, 130, math.pi / 2, '', 5000 - self.player.camera_x, 190, 250, 100)
+            for i in range(1000, 5000 - 1, 500):
+                Running_track.obstacle.draw(i - self.player.camera_x, 120, 100, 100)
+            self.arrow_draw()
 
     def arrow_draw(self):
-        for i in range(0, len(Running_track.command)):
-            if Running_track.command[i] == 0:
-                self.arrow.clip_composite_draw(0, 0, 670, 373, 0, ' ',
+        if self.track_num == 20:
+            for i in range(0, len(Running_track.command)):
+                if Running_track.command[i] == 0:
+                    Running_track.arrow.clip_composite_draw(0, 0, 670, 373, 0, ' ',
                                                self.player.x + i * 100 - 50 - self.player.camera_x,
                                                self.player.y + 100, 100, 100)
-            elif Running_track.command[i] == 1:
-                self.arrow.clip_composite_draw(0, 0, 670, 373, 0, 'h',
+                elif Running_track.command[i] == 1:
+                    Running_track.arrow.clip_composite_draw(0, 0, 670, 373, 0, 'h',
                                                self.player.x + i * 100 - 50 - self.player.camera_x,
                                                self.player.y + 100, 100, 100)
-            elif self.command[i] == 2:
-                self.arrow.clip_composite_draw(0, 0, 670, 373, math.pi / 2, '',
+                elif Running_track.command[i] == 2:
+                    Running_track.arrow.clip_composite_draw(0, 0, 670, 373, math.pi / 2, '',
                                                self.player.x + i * 100 - 50 - self.player.camera_x,
                                                self.player.y + 100, 100, 100)
-            elif self.command[i] == 3:
-                self.arrow.clip_composite_draw(0, 0, 670, 373, math.pi / 2, 'h',
+                elif Running_track.command[i] == 3:
+                    Running_track.arrow.clip_composite_draw(0, 0, 670, 373, math.pi / 2, 'h',
                                                self.player.x + i * 100 - 50 - self.player.camera_x,
                                                self.player.y + 100, 100, 100)
 
