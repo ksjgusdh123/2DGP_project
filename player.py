da0d4e1272fa97af3f1c116963cf822850e2c4aa
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-10-17 14:40:50 +0900

    player움직이도록 RUN클래스 추가하고 statemachine수정

--- player.py
+++ player.py
@@ -1,13 +1,28 @@
-from pico2d import load_image
+from pico2d import *
+
+sonic_run = [7, 31, 56, 82, 105, 130, 154, 180]
+
+def a_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
+
+def a_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_a
+
+def d_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_d
+
+def d_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_d
+
 
 class Idle:
     @staticmethod
-    def enter(player):
+    def enter(player, e):
         player.frame = 0
         player.action = 1
         pass
     @staticmethod
-    def exit(player):
+    def exit(player, e):
         pass
     @staticmethod
     def do(player):
@@ -15,13 +30,49 @@ class Idle:
     @staticmethod
     def draw(player):
         player.image.clip_draw(player.frame // 2 * 22 + 5, 249, 18, 30, player.x, player.y, 50, 100)
+
+class Run:
+    @staticmethod
+    def enter(player, e):
+        if d_down(e):
+            player.dir, player.action = 1, 1
+        elif a_down(e):
+            player.dir, player.action = -1, 0
+        pass
+    @staticmethod
+    def exit(player, e):
+        pass
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + 1) % 8
+        player.x += player.dir * 5
+    @staticmethod
+    def draw(player):
+        if player.dir > 0:
+            player.image.clip_draw(sonic_run[player.frame], 149, 23, 27, player.x, player.y, 50, 100)
+        elif player.dir < 0:
+            player.image.clip_composite_draw(sonic_run[player.frame], 149, 23, 27, 0, 'h', player.x,
+                                             player.y, 50, 100)
 class StateMachine:
     def __init__(self, player):
         self.player = player
         self.cur_state = Idle
+        self.transitions = {
+            Idle: {a_down: Run, d_down: Run, a_up: Idle, d_up: Idle},
+            Run: {a_down: Idle, d_down: Idle, a_up: Idle, d_up: Idle}
+        }
 
     def start(self):
-        self.cur_state.enter(self.player)
+        self.cur_state.enter(self.player, ('NONE', 0))
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.player, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.player, e)
+                return True
+        return False
 
     def update(self):
         self.cur_state.do(self.player)
@@ -34,6 +85,7 @@ class Player:
         self.image = load_image('sonic_animation.png')
         self.frame = 0
         self.action = 1
+        self.dir = 0
         self.x, self.y = 100, 100
         self.state_machine = StateMachine(self)
         self.state_machine.start()
@@ -41,6 +93,7 @@ class Player:
     def update(self):
         self.state_machine.update()
     def handle_event(self,event):
+        self.state_machine.handle_event(('INPUT', event))
         pass
     def draw(self):
         self.state_machine.draw()
