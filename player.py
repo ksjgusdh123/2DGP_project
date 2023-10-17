877f9e42db50990590fc196a2ed2c5656ecc3249
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-10-17 14:13:24 +0900

    player가 state_machine을 이용해 작동되도록 작성, idle상태에 대한 클래스 작성

--- player.py
+++ player.py
@@ -1,12 +1,46 @@
 from pico2d import load_image
 
+class Idle:
+    @staticmethod
+    def enter(player):
+        player.frame = 0
+        player.action = 1
+        pass
+    @staticmethod
+    def exit(player):
+        pass
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + 1) % 10
+    @staticmethod
+    def draw(player):
+        player.image.clip_draw(player.frame // 2 * 22 + 5, 249, 18, 30, player.x, player.y, 50, 100)
+class StateMachine:
+    def __init__(self, player):
+        self.player = player
+        self.cur_state = Idle
+
+    def start(self):
+        self.cur_state.enter(self.player)
+
+    def update(self):
+        self.cur_state.do(self.player)
+
+    def draw(self):
+        self.cur_state.draw(self.player)
 
 class Player:
     def __init__(self):
         self.image = load_image('sonic_animation.png')
         self.frame = 0
+        self.action = 1
         self.x, self.y = 100, 100
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+
     def update(self):
-        self.frame = (self.frame + 1) % 10
+        self.state_machine.update()
+    def handle_event(self,event):
+        pass
     def draw(self):
-        self.image.clip_draw(self.frame // 2 * 22 + 5, 249, 18, 30, self.x, self.y, 50, 100)
+        self.state_machine.draw()
