7cac01a723d72081e19dd2900c59cbdc2a63893a
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-10-17 13:58:24 +0900

    화면에 캐릭터 출력

--- sonic.py
+++ sonic.py
@@ -1,5 +1,14 @@
 from pico2d import *
 
+class Player:
+    def __init__(self):
+        self.image = load_image('sonic_animation.png')
+        self.frame = 0
+        self.x, self.y = 100, 100
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+    def draw(self):
+        self.image.clip_draw(self.frame // 2 * 22 + 5, 249, 18, 30, self.x, self.y, 50, 100)
 def handle_event():
     global running
 
@@ -13,9 +22,11 @@ def handle_event():
 running = True
 
 open_canvas()
-
+player = Player()
 while running:
+    player.draw()
     handle_event()
+    update_canvas()
     pass
 
 close_canvas()
