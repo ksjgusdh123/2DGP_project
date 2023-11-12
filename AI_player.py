61afb75eb0314e6af0383b3e2df5b5f8ad73cefd
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-11-13 02:48:57 +0900

    AI에 대해서 수영 애니메이션 수행, 함수의 코드를 정리

--- AI_player.py
+++ AI_player.py
@@ -13,6 +13,11 @@ TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
 
+SONIC = 0
+TAILS = 1
+SHADOW = 2
+ECHDNA = 3
+
 
 class AI:
     ai_num = 0
@@ -21,7 +26,7 @@ class AI:
     def __init__(self, player):
         self.player = player
 
-        if self.player.character_id == 0:
+        if self.player.character_id == SONIC:
             if AI.ai_num == 0:
                 AI.ai_num += 1
                 self.image = load_image('image/tails.png')
@@ -29,7 +34,7 @@ class AI:
                 self.image = load_image('image/shadow.png')
             elif AI.ai_num == 3:
                 self.image = load_image('image/echidna.png')
-        elif self.player.character_id == 1:
+        elif self.player.character_id == TAILS:
             if AI.ai_num == 0:
                 self.image = load_image('image/sonic_animation.png')
             elif AI.ai_num == 1:
@@ -37,7 +42,7 @@ class AI:
                 self.image = load_image('image/shadow.png')
             elif AI.ai_num == 3:
                 self.image = load_image('image/echidna.png')
-        elif self.player.character_id == 2:
+        elif self.player.character_id == SHADOW:
             if AI.ai_num == 0:
                 self.image = load_image('image/sonic_animation.png')
             elif AI.ai_num == 1:
@@ -45,7 +50,7 @@ class AI:
             elif AI.ai_num == 2:
                 AI.ai_num += 1
                 self.image = load_image('image/echidna.png')
-        elif self.player.character_id == 3:
+        elif self.player.character_id == ECHDNA:
             if AI.ai_num == 0:
                 self.image = load_image('image/sonic_animation.png')
             elif AI.ai_num == 1:
@@ -69,50 +74,86 @@ class AI:
         self.draw_character()
 
     def draw_character(self):
-        if self.ch_id == 0:
+        if self.ch_id == SONIC:
             if not self.player.start:
-                self.image.clip_draw(int(self.frame) // 2 * 22 + 5, 249, 18, 30, self.x - self.player.camera_x, self.y, 50, 100)
+                self.image.clip_draw(int(self.frame) // 2 * 22 + 5, 249, 18, 30, self.x - self.player.camera_x, self.y,
+                                     50, 100)
             else:
-                if self.jump:
-                    self.image.clip_draw(sonic_jump[int(self.frame)], 215, sonic_jump_w[int(self.frame)], 30,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
-                else:
-                    self.image.clip_draw(sonic_run[int(self.frame)], 149, 23, 27, self.x - self.player.camera_x, self.y, 50, 100)
+                if self.mode == 'run':
+                    if self.jump:
+                        self.image.clip_draw(sonic_jump[int(self.frame)], 215, sonic_jump_w[int(self.frame)], 30,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                    else:
+                        self.image.clip_draw(sonic_run[int(self.frame)], 149, 23, 27, self.x - self.player.camera_x,
+                                             self.y, 50, 100)
+                elif self.mode == 'swim':
+                    self.image.clip_draw(sonic_swim[int(self.frame)], 57, 36, 20,
+                                                self.x - self.player.camera_x,
+                                                self.y, 50, 100)
 
-        elif self.ch_id == 1:
+        elif self.ch_id == TAILS:
             if not self.player.start:
-                self.image.clip_draw(int(self.frame) // 2 * 32 + 107, 960, 21, 35, self.x - self.player.camera_x, self.y, 50, 100)
+                self.image.clip_draw(int(self.frame) // 2 * 32 + 107, 960, 21, 35, self.x - self.player.camera_x,
+                                     self.y, 50, 100)
             else:
-                if self.jump:
-                    self.image.clip_draw(tails_jump[int(self.frame)], 736, tails_jump_w[int(self.frame)], 32,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
-                else:
-                    self.image.clip_draw(tails_run[int(self.frame)], 784, 36, 35, self.x - self.player.camera_x, self.y, 50, 100)
+                if self.mode == 'run':
+                    if self.jump:
+                        self.image.clip_draw(tails_jump[int(self.frame)], 736, tails_jump_w[int(self.frame)], 32,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                    else:
+                        self.image.clip_draw(tails_run[int(self.frame)], 784, 36, 35, self.x - self.player.camera_x,
+                                             self.y, 50, 100)
+                elif self.mode == 'swim':
+                    self.image.clip_composite_draw(tails_swim[int(self.frame)], 264, 40, 30, 3.14 * 30, 'h',
+                                                          self.x - self.player.camera_x, self.y, 50, 100)
 
-        elif self.ch_id == 2:
-            if not self.player.start:
-                self.image.clip_draw(int(self.frame) // 2 * 26 + 6, 467, 23, 33, self.x - self.player.camera_x, self.y, 50, 100)
-            else:
-                if self.jump:
-                    self.image.clip_draw(shadow_jump[int(self.frame)], 317, shadow_jump_w[int(self.frame)], 39,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
-                else:
-                    self.image.clip_draw(shadow_run[int(self.frame)], 431, shadow_run_w[int(self.frame)], 32,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
 
-        if self.ch_id == 3:
+        elif self.ch_id == SHADOW:
             if not self.player.start:
-                self.image.clip_draw(int(self.frame) // 2 * 31 + 117, 262, 29, 42, self.x - self.player.camera_x, self.y, 50, 100)
+                self.image.clip_draw(int(self.frame) // 2 * 26 + 6, 467, 23, 33, self.x - self.player.camera_x, self.y,
+                                     50, 100)
             else:
-                if self.jump:
-                    self.image.clip_draw(ech_jump[int(self.frame)], 214, ech_jump_w[int(self.frame)], 45,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
-                else:
-                    self.image.clip_draw(ech_run[int(self.frame)], 218, ech_run_w[int(self.frame)], 40,
-                                         self.x - self.player.camera_x, self.y, 50, 100)
+                if self.mode == 'run':
+                    if self.jump:
+                        self.image.clip_draw(shadow_jump[int(self.frame)], 317, shadow_jump_w[int(self.frame)], 39,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                    else:
+                        self.image.clip_draw(shadow_run[int(self.frame)], 431, shadow_run_w[int(self.frame)], 32,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                elif self.mode == 'swim':
+                    self.image.clip_composite_draw(shadow_swim[int(self.frame)], 284, 38, 30, 3.14 * 30, 'h',
+                                                          self.x - self.player.camera_x, self.y, 50, 100)
+
+        if self.ch_id == ECHDNA:
+            if not self.player.start:
+                self.image.clip_draw(int(self.frame) // 2 * 31 + 117, 262, 29, 42, self.x - self.player.camera_x,
+                                     self.y, 50, 100)
+            else:
+                if self.mode == 'run':
+                    if self.jump:
+                        self.image.clip_draw(ech_jump[int(self.frame)], 214, ech_jump_w[int(self.frame)], 45,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                    else:
+                        self.image.clip_draw(ech_run[int(self.frame)], 218, ech_run_w[int(self.frame)], 40,
+                                             self.x - self.player.camera_x, self.y, 50, 100)
+                elif self.mode == 'swim':
+                    self.image.clip_draw(ech_swim[int(self.frame)], 51, 50, 25,
+                                                self.x - self.player.camera_x,
+                                                self.y, 50, 100)
 
     def update(self):
         # 게임 시작 시 sonic
+        if self.mode == 'run':
+            self.running_track_update()
+        elif self.mode == 'swim':
+            self.swim_update()
+
+    def swim_update(self):
+        if self.player.start:
+            self.x += self.speed * RUN_SPEED_PPS * game_framework.frame_time
+        self.basic_update()
+
+    def running_track_update(self):
         if self.player.start:
             if self.jump:
                 self.x += 1 * RUN_SPEED_PPS * game_framework.frame_time
@@ -129,12 +170,15 @@ class AI:
                 self.exceed_point += 500
             elif self.jump and self.x - self.exceed_point > 50:
                 self.y -= 1 * RUN_SPEED_PPS * game_framework.frame_time
+        self.basic_update()
 
+
+    def basic_update(self):
         if self.ch_id == 1 and not self.player.start:
             self.frame = (self.frame + 14 * ACTION_PER_TIME * game_framework.frame_time) % 14
         else:
             self.frame = (self.frame + 10 * ACTION_PER_TIME * game_framework.frame_time) % 10
-
         if self.x >= 5000 and self.finish == False:
             self.finish = True
             print(get_time() - self.time)
+
