cfafb7895e19134395c9dcd25c2bd70ca4423fe0
hyundojeong <122209567+ksjgusdh123@users.noreply.github.com>
2023-11-17 10:19:51 +0900

    캐릭터 선택창에서 마우스 커서의 위치에 따라 배너의 알파값을 부여해 선택하는 부분을 표시해 주었다.

--- character_select_mode.py
+++ character_select_mode.py
@@ -16,6 +16,17 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_MOUSEMOTION:
+            num = 0
+            if HEIGHT - event.y < 150:
+                num = 3
+            elif HEIGHT - event.y < 300 and HEIGHT - event.y > 150:
+                num = 2
+            elif HEIGHT - event.y < 450 and HEIGHT - event.y > 300:
+                num = 1
+            elif HEIGHT - event.y < 600 and HEIGHT - event.y > 450:
+                num = 0
+            check_alpha(num)
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if HEIGHT - event.y < 150:
                 character_num = 3
@@ -31,14 +42,23 @@ def handle_events():
                 game_framework.change_mode(run_track_mode)
 
 
+def check_alpha(num):
+    for i in range(0, 4):
+        if i == num:
+            alpha[i] = 255
+        else:
+            alpha[i] = 128
+
+
 def init():
     global banner_image
     global running
     global character_num
-
+    global alpha
     character_num = None
     banner_image = load_image('image/banner.png')
     running = True
+    alpha = [128, 128, 128, 128]
 
 def finish():
     global banner_image
@@ -49,9 +69,13 @@ def update():
 
 def draw():
     clear_canvas()
+    banner_image.opacify(alpha[0] / 255.0)
     banner_image.clip_draw(584, 1202, 288, 72, 400, 525, 800, 150)
+    banner_image.opacify(alpha[1] / 255.0)
     banner_image.clip_draw(875, 1202, 288, 72, 400, 375, 800, 150)
+    banner_image.opacify(alpha[2] / 255.0)
     banner_image.clip_draw(2, 1052, 288, 72, 400, 225, 800, 150)
+    banner_image.opacify(alpha[3] / 255.0)
     banner_image.clip_draw(2, 1127, 288, 72, 400, 75, 800, 150)
     update_canvas()
 
